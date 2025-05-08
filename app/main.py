0693a1e6e11705082fa4b494a871452d66c0c62d
memento-a25 <grekhnev.george@gmail.com>
2025-05-08 20:37:16 +0300

    Initial commit

---
+++ app/main.py
@@ -0,0 +1,19 @@
+from flask import Flask
+from prometheus_client import Counter, genereate_latest, CONTENT_TYPE_LATEST
+import time
+
+app = Flask(__name__)
+
+REQUEST_COUNT = Counter('app_requests_total', 'Total number of requests')
+
+@app.route('/')
+def hello():
+  REQUESTS_COUNT.inc()
+  return "Hello from Flask-app!"
+
+@app.router('/metrics')
+def metrics():
+  return generate_latest(), 200, {'Content-Type': CONTENT_TYPE_LATEST}
+
+if __name__ == '__main__':
+  app.run(host='0.0.0.0', port=5000)
