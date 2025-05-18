b4931de8c959215a436405dd974706d4b84ecc05
memento-a25 <grekhnev.george@gmail.com>
2025-05-18 12:32:12 +0300

    Update test_integration.py

--- app/tests/test_integration.py
+++ app/tests/test_integration.py
@@ -3,7 +3,7 @@ import requests
 
 def test_grafana_prometheus_connection():
     response = requests.get(
-        "http://localhost:3000/api/datasources",
+        "http://localhost:3001/api/datasources",
         auth=("admin", "admin")  # Дефолтные креды Grafana
     )
     
@@ -12,4 +12,4 @@ def test_grafana_prometheus_connection():
     prometheus_ds = [ds for ds in datasources if ds["type"] == "prometheus"]
     
     assert len(prometheus_ds) > 0, "Prometheus datasource not found"
-    assert prometheus_ds[0]["url"] == "http://prometheus:9090", "Wrong Prometheus URL"
+    assert prometheus_ds[0]["url"] == "http://prometheus:9091", "Wrong Prometheus URL"
